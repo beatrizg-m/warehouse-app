18cf12a41e2a35435429a46ad085f80e13e09ff9
beatriz <beatrizg.m@yahoo.com>
2023-04-20 18:42:32 -0300

    Usuario consegue ver galpoes cadastrados

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -14,4 +14,37 @@ describe 'Usuario visita tela inicial' do
     expect(page).to have_content('Galpoes & Estoque')
   end
 
+  it 'e ve os galpoes cadastrados' do
+    #Arrange
+    #cadastrar 2 galpoes: Rio e Maceio
+    Warehouse.create(name: 'Rio', code:'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code:'MCZ', city: 'Maceio', area: 50_000)
+    #Act
+    visit('/')
+
+    #Assert
+    #garantir que vejo na tela os 2 galpoes: Rio e Maceio
+    #e neste caso, vamos ter vários expect porque temos varias funcionalidades, no caso queremos ver
+    #nome, metragem, localidade e etc.
+    expect(page).not_to have_content('Nao existem galpoes cadastrados')
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Codígo: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m2')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Codígo: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m2')
+  end
+
+  it 'e nao existem galpoes cadastrados' do
+    #Arrange
+
+    #Act
+    visit('/')
+    #Assert
+    expect(page).to have_content('Nao existem galpoes cadastrados')
+  end
+
 end
