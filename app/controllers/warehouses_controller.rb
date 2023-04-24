aad4fd0bfb4f22f7d29c692b320a733ffec2cd7e
beatriz <beatrizg.m@yahoo.com>
2023-04-24 12:08:16 -0300

    flat

---
+++ app/controllers/warehouses_controller.rb
@@ -0,0 +1,6 @@
+class WarehousesController < ApplicationController
+
+    def show 
+    end
+
+end
--- spec/system/user_view_warehouse_details_spec.rb
+++ spec/system/user_view_warehouse_details_spec.rb
@@ -3,10 +3,20 @@ require "rails_helper"
 describe 'Usuario ve detalhe de um galpao ' do
   it ' e ve informacoes adicionais' do
     Warehouse.create(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area:100_000,
-                      address: 'Avenida do aeroporto, 1000', CEP: '12234-000',
+                      address: 'Avenida do aeroporto, 1000', cep: '12234-000',
                       description: 'Galpao destinado para cargas internacionais')
 
     #Act
+    visit('/')
+    click_on('Aeroporto SP')
 
+    #Assert 
+    expect(page).to have_content('Galpao Aeroporto SP')
+    expect(page).to have_content('Codigo: GRU')
+    expect(page).to have_content('Cidade: Guarulhos')
+    expect(page).to have_content('Area: 100000')
+    expect(page).to have_content('Endereco: avenida do aeroporto, 1000')
+    expect(page).to have_content('CEP: 12234-000')
+    expect(page).to have_content('Descricao: Galpao destinado para cargas internacionais')
   end
 end
