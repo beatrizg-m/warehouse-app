7f8d9d62e5b24661b4d83fb0369f2090d2f6af5d
beatriz <beatrizg.m@yahoo.com>
2023-04-25 11:17:45 -0300

    Usuario cadastra novo galpao

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -4,4 +4,17 @@ class WarehousesController < ApplicationController
         @warehouse = Warehouse.find(params[:id])
     end
 
+    def new
+    end
+
+    def create
+        #receber dados enviados e criar um novo galpao
+        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description,
+                                                            :address, :cep, :area) #Strong Parameters
+
+        w = Warehouse.new(warehouse_params)
+        w.save()
+
+        redirect_to root_path
+    end
 end
---
+++ spec/system/user_register_warehouse_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'usuario cadastra um galpao' do
+  it 'a partir da tela inicial' do
+    #Arrange
+
+    #Act
+    visit root_path
+    click_on 'Cadastrar Galpao'
+    #Assert
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Descricao')
+    expect(page).to have_field('Codigo')
+    expect(page).to have_field('Endereco')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Area')
+  end
+
+  it 'com sucesso' do
+    #Arrange
+
+    #Act
+    visit root_path
+    click_on 'Cadastrar Galpao'
+    fill_in 'Nome', with: 'Rio de Janeiro'
+    fill_in 'Descricao', with: 'Galpao da zona portuaria do Rio'
+    fill_in 'Codigo', with: 'RIO'
+    fill_in 'Endereco', with: 'Avenida do Museu do amanha, 1000'
+    fill_in 'Cidade', with: 'Rio de Janeiro'
+    fill_in 'CEP', with: '20100-000'
+    fill_in 'Area', with: '32000'
+    click_on 'Enviar'
+
+    #Assert
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Galpao cadastrado com sucesso.'
+    expect(page).to have_content 'Rio de Janeiro'
+    expect(page).to have_content 'RIO'
+    expect(page).to have_content '32000 m2'
+  end
+end
