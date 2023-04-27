823f6d915e7c65462a7bb1155f96fc69069e6678
beatriz <beatrizg.m@yahoo.com>
2023-04-27 12:39:41 -0300

    Melhorias no Controller e nas views

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,17 +1,14 @@
 class WarehousesController < ApplicationController
 
-    def show
-        @warehouse = Warehouse.find(params[:id])
-    end
+    before_action :set_warehouse, only: [:show, :edit, :update]
+
+    def show; end
 
     def new
         @warehouse = Warehouse.new
     end
 
     def create
-        warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description,
-                                                            :address, :cep, :area)
-
         @warehouse = Warehouse.new(warehouse_params)
         if @warehouse.save()
             redirect_to root_path, notice: 'Galpao cadastrado com sucesso.'
@@ -19,24 +16,27 @@ class WarehousesController < ApplicationController
             flash.now[:notice] = 'Galpao nao cadastrado'
             render 'new'
         end
-
     end
 
-    def edit
-        @warehouse = Warehouse.find(params[:id])
-    end
+    def edit; end
 
     def update
-        @warehouse = Warehouse.find(params[:id])
-
-        warehouse_params = params.require(:warehouse).permit(:name, :code,  :city, :description,
-            :address, :cep, :area)
-
         if @warehouse.update(warehouse_params)
             redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso'
         else
             flash[:notice] = 'Não foi possível atualizar o galpão'
             render 'edit'
         end
+    end
+
+    private
+
+    def set_warehouse
+        @warehouse = Warehouse.find(params[:id])
+    end
+
+    def warehouse_params
+        params.require(:warehouse).permit(:name, :code,  :city, :description,
+                                        :address, :cep, :area)
     end
 end
