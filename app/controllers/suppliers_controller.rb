fccf5e55bb02cc8f940f5bcc488059d6381c92fc
beatriz <beatrizg.m@yahoo.com>
2023-04-28 17:13:40 -0300

    Usuário vê todos o Fornecedores(Suppliers) cadastrados

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,6 +1,9 @@
 class SuppliersController < ApplicationController
 
   def index
-
+    @suppliers = Supplier.all
+    if @suppliers.empty?
+      flash[:notice] = 'Não existem fornecedores cadastrados'
+    end
   end
 end
--- spec/system/suppliers/user_view_suppliers_spec.rb
+++ spec/system/suppliers/user_view_suppliers_spec.rb
@@ -1,13 +1,37 @@
 require 'rails_helper'
 
 describe 'usuário entra na página de fornecedores' do
-  it 'e vê todos os fornecedores' do
+  it 'e vê todos os fornecedores registrados' do
     #Arrange
-
+    supplier_one = Supplier.create!(corporate_name:'ACME LTDA', brand_name: 'ACME', registration_number: '2345678912', full_address: 'Av das Ubaias, 50', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+    supplier_two = Supplier.create!(corporate_name:'Ponyo Industria Brasil', brand_name: 'Ponyo', registration_number: '2000078912', full_address: 'Av do mar, 150', city:  'Recife', state: 'PE', email: 'ponyobrasil@gmail.com')
     #Act
     visit root_path
     click_on 'Fornecedores'
     #Assert
     expect(current_path).to eq(suppliers_path)
+    expect(page).to have_content 'Fornecedor: ACME'
+    expect(page).to have_content 'Número de registro: 2345678912'
+    expect(page).to have_content 'Fornecedor: Ponyo'
+    expect(page).to have_content 'Número de registro: 2000078912'
+  end
+
+  it 'e volta para a página inicial' do
+  #Arrange
+  #Act
+  visit root_path
+  click_on 'Fornecedores'
+  click_on 'Voltar'
+  #Assert
+  expect(current_path).to eq root_path
+  end
+
+  it 'e não existem fornecedores cadastrados' do
+    #Arrange
+    #Act
+    visit root_path
+    click_on 'Fornecedores'
+    #Assert
+    expect(page).to have_content 'Não existem fornecedores cadastrados'
   end
 end
